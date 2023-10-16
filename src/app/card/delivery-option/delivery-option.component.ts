7eff4aca73650a45ad8c82a8f505ed38aaecf5d0
Oleg Stelmakh <oleg.stelmakh@kinseed.com>
2023-10-16 18:57:51 +0400

    Add delivery options

--- src/app/card/delivery-option/delivery-option.component.ts
+++ src/app/card/delivery-option/delivery-option.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { Input } from '@angular/core';
+import { DeliveryOptions } from 'src/app/types/card';
 
 @Component({
   selector: 'app-delivery-option',
-  templateUrl: './delivery-option.component.html',
+  template: `
+    <div class="delivery__option">
+      Deliver to your home
+      <em>{{options?.delivery}}</em>
+    </div>
+    <div class="delivery__option">
+      Deliver to shop
+      <em>{{ postamate }}</em>
+    </div>
+    <div class="delivery__option">
+      In Stock
+      <em>{{options?.available}} pc.</em>
+    </div>
+  `,
   styleUrls: ['./delivery-option.component.scss']
 })
 export class DeliveryOptionComponent {
+  @Input() options?: DeliveryOptions
 
+  get postamate(): string {
+    return this.options?.postamate ? 'available' : 'not available';
+  }
 }
