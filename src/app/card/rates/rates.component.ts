10e179575b267de9fff366021235bfa732fefbc0
Oleg Stelmakh <oleg.stelmakh@kinseed.com>
2023-10-26 00:03:22 +0400

    Update rating component

--- src/app/card/rates/rates.component.ts
+++ src/app/card/rates/rates.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-rates',
   templateUrl: './rates.component.html',
   styleUrls: ['./rates.component.scss']
 })
-export class RatesComponent {
+export class RatesComponent implements OnInit {
+
+  @Input() maxStars?: number;
+  @Output() ratingChanged = new EventEmitter<number>();
+
+  rating: number = 0;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  setRating(starNumber: number): void {
+    this.rating = starNumber;
+    this.ratingChanged.emit(this.rating);
+  }
+
+  isStarFilled(starNumber: number): boolean {
+    return starNumber <= this.rating;
+  }
 
 }
